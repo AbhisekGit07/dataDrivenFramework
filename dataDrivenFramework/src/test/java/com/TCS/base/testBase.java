de8a956f62d994270afb06d934cd56a8bba9876e
rima <rima@Bidisha>
2019-08-08 09:49:47 +0530

    Removing warning from Base class

--- dataDrivenFramework/src/test/java/com/TCS/base/testBase.java
+++ dataDrivenFramework/src/test/java/com/TCS/base/testBase.java
@@ -10,7 +10,6 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
