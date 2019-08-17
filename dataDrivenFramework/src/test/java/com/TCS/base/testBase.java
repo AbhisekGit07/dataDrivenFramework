dc5ccf884a317719a97a30343464d95e2962efdd
rima <rima@Bidisha>
2019-08-17 17:36:20 +0530

    FB Reg Added

--- dataDrivenFramework/src/test/java/com/TCS/base/testBase.java
+++ dataDrivenFramework/src/test/java/com/TCS/base/testBase.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.AfterSuite;
+//import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class testBase {
